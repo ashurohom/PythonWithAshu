079a655a579190f3bcba7dacbcf6af5ea764971a
ashurohom <ashitosh.rohom@gmail.com>
2024-11-24 19:44:44 +0530

    Commit

--- Ashu/test.py
+++ Ashu/test.py
@@ -1,25 +1,3 @@
-# inheritance
+sqr=[x*x for x in range(1,5)]
+print(sqr)
 
-class Laptop:
-    def __init__(self,name,price):
-        self.name=name
-        self.price=price
-
-class Parts(Laptop):
-    def __init__(self,name,price,ssd):
-        super().__init__(name,price)
-        self.ssd=ssd
-
-NewParts = Parts("ASUS",78000,"1TB")
-print(f'Name {NewParts.name}')
-print(f'Price {NewParts.price}')
-print(f'SSD {NewParts.ssd}')
-
-
-# MyLaptop = Laptop("ASUS",78000)
-# print(MyLaptop.name)
-# print(MyLaptop.price)
-
-x=10
-y=20
-print(x>y or x==10)

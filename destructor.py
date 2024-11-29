9d7e6efb04039668b630ef077d5370373ba15b6b
ashurohom <ashitosh.rohom@gmail.com>
2024-11-29 15:11:36 +0530

    initial commit

---
+++ destructor.py
@@ -0,0 +1,16 @@
+class A:
+    def __init__(self,name):
+        self.name=name
+        print("Ashu")
+
+    def display(self):
+        return self.name   
+    
+    def __del__(self):
+        print("Object Deleted")
+
+obj = A("AshuRohom")    
+print(obj.display())
+# del obj 
+print("After Object Delete")
+#destructor call when object is deleted but vs code has issue

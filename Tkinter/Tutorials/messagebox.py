ef3747d518941d4f99e5459865fb8e7184e97f15
ashurohom <ashitosh.rohom@gmail.com>
2024-11-29 23:32:03 +0530

    add msgbox

--- Tkinter/Tutorials/messagebox.py
+++ Tkinter/Tutorials/messagebox.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter.messagebox as tmsg
 window = Tk()
 window.geometry("655x455")
 window.title("Ashu Rohom")
@@ -6,43 +7,33 @@ window.title("Ashu Rohom")
 def myfun():
     print("MyFun")
 
+#open new dialogbox
 def help():
     print("How I Can Help You..?")
-        
+    tmsg.showinfo("Help", "Ashu Will Help You")
 
-#used to create a menubar
-mymenu = Menu(window)
-mymenu.add_command(label="File",command=myfun)
-mymenu.add_command(label="File",command=quit)
-window.config(menu=mymenu)
+
+#Yes No Button
+def rate():
+    print("Rate Us")  
+    value = tmsg.askquestion("Tell Me","Love or Not")    
+    print(value)
+
+    if value == "yes":
+        msg = "Great,, Awesome"
+    else:
+        msg = "We Will Upgrade"  
+
+    tmsg.showinfo("Exp",msg)     
+
+
 
 #menubar with list
 
 menubar = Menu(window)
-m1 = Menu(menubar, tearoff=0)
-m1.add_command(label="New",command=myfun)
-m1.add_command(label="Save",command=myfun)
-m1.add_command(label="Save As",command=myfun)
-
-#make a line seprate
-m1.add_separator()
-
-m1.add_command(label="Print",command=myfun)
-m1.add_command(label="Delete",command=myfun)
-window.config(menu=menubar)
-menubar.add_cascade(label="File",menu=m1)
-
-m2 = Menu(menubar, tearoff=0)
-m2.add_command(label="Cut",command=myfun)
-m2.add_command(label="Copy",command=myfun)
-m2.add_command(label="Paste",command=myfun)
-m2.add_command(label="Find",command=myfun)
-m2.add_command(label="More",command=myfun)
-window.config(menu=menubar)
-menubar.add_cascade(label="Edit",menu=m2)
-
 m3 = Menu(menubar, tearoff=0)
 m3.add_command(label="Help",command=help)
+m3.add_command(label="RateUs",command=rate)
 window.config(menu=menubar)
 menubar.add_cascade(label="Help",menu=m3)
 

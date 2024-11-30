3334aedede5bbe96d7912d8668dc7ed19e064134
ashurohom <ashitosh.rohom@gmail.com>
2024-11-30 13:27:17 +0530

    update colse btn

--- Tkinter/Tutorials/auto_height_width.py
+++ Tkinter/Tutorials/auto_height_width.py
@@ -1,23 +1,30 @@
 from tkinter import *
+
 window = Tk()
 
 def changesize():
     window.geometry(f'{high.get()}x{wid.get()}')
 
+window.title("CodeWithAshu - HeightWidth")
+window.wm_iconbitmap("icon.png")
+window.configure(background="green")
+
 frame1 = Frame(window)
-Label(frame1,text="Width : ").grid(row=0,column=1)
-Label(frame1,text="Height : ").grid(row=1,column=1)
+Label(frame1, text="Width : ").grid(row=0, column=1)
+Label(frame1, text="Height : ").grid(row=1, column=1)
 
 height = StringVar()
-high = Entry(frame1,textvariable=height)
-high.grid(row=0,column=2)
+high = Entry(frame1, textvariable=height)
+high.grid(row=0, column=2)
 
 width = StringVar()
-wid = Entry(frame1,textvariable=width)
-wid.grid(row=1,column=2)
+wid = Entry(frame1, textvariable=width)
+wid.grid(row=1, column=2)
 
-b1 = Button(window,text="Apply",command=changesize)
+b1 = Button(window, text="Apply", command=changesize)
 b1.pack(side=BOTTOM)
+
 frame1.pack(side=TOP)
+Button(window, text="Close", command=window.destroy).pack()
 
 window.mainloop()

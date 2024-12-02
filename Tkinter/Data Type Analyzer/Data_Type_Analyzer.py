835c02757b529f183b959a6cc587153eb5f4a42e
ashurohom <ashitosh.rohom@gmail.com>
2024-12-02 16:00:26 +0530

    update symbol value

--- Tkinter/Data Type Analyzer/Data_Type_Analyzer.py
+++ Tkinter/Data Type Analyzer/Data_Type_Analyzer.py
@@ -8,17 +8,20 @@ def checkval():
     input_value = ValueEntry.get()  
     ValueAnswer.delete(0, END)  
 
-    if input_value.isdigit():  
+    if input_value.lower() in ['true', 'false']:
+            ValueAnswer.insert(0, "Boolean")
+    elif input_value.isdigit():  
         ValueAnswer.insert(0, "Number")
     elif input_value.isalpha():  
         ValueAnswer.insert(0, "String")
     elif input_value.isalnum():  
         ValueAnswer.insert(0, "Alphanumeric")
+    elif input_value.lower() in ['+','-','*','/','!','@','#','$','%','^',',&',"?"]:
+         ValueAnswer.insert(0, "Symbol")    
     else: 
-        ValueAnswer.insert(0, "Symbol")
+        ValueAnswer.insert(0, "Invalid Input")
 
 
-# Frame f1 (title section)
 f1 = Frame(window)
 f1.grid(row=0, column=0, columnspan=3, pady=20)
 
@@ -32,32 +35,30 @@ l2.grid(row=1, column=0)
 f2 = Frame(window)
 f2.grid(row=1, column=0, columnspan=3, pady=20)
 
+Note = Label(f2, text="Number String Boolean Symbol Alphanumeric", font="comicsansms 10 bold", fg="yellow")
+Note.grid(row=0, column=1, padx=10, pady=5)
 
-UserInput = Label(f2, text="Enter Value : ", font="comicsansms 10 bold")
-UserInput.grid(row=0, column=0, padx=10)  
+UserInput = Label(f2, text="Enter Value : ", font="comicsansms 10 bold", fg="Black")
+UserInput.grid(row=1, column=0, padx=10)  
 
 ValueEntry = Entry(f2)
-ValueEntry.grid(row=0, column=1, padx=10)  
+ValueEntry.grid(row=1, column=1, padx=10)  
 
 
-AnswerLable = Label(f2, text="Answer : ", font="comicsansms 10 bold")
-AnswerLable.grid(row=1, column=0, padx=10)
+AnswerLable = Label(f2, text="Answer : ", font="comicsansms 10 bold", fg="Black")
+AnswerLable.grid(row=2, column=0, padx=10)
 
 ValueAnswer = Entry(f2)
-ValueAnswer.grid(row=1, column=1, columnspan=2, pady=10)
+ValueAnswer.grid(row=2, column=1, columnspan=2, pady=10)
 
 
-
-# Button to check result
 b1 = Button(f2, text="Check", command=checkval, bg="Black", fg="White",font="comicsansms 10 bold", border=2, relief=RIDGE)
-b1.grid(row=2, column=0, pady=10)
+b1.grid(row=3, column=1, pady=10)
 
 b2 = Button(f2, text="Exit", command=quit, bg="Black", fg="White", font="comicsansms 10 bold", border=2, relief=RIDGE)
-b2.grid(row=2, column=1, pady=10)
+b2.grid(row=4, column=1, pady=10)
 
 
-
-# Center content by configuring grid columns
 window.grid_columnconfigure(0, weight=1)
 window.grid_columnconfigure(1, weight=1)
 window.grid_columnconfigure(2, weight=1)

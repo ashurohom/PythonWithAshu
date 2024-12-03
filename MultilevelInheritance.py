388cd0b08a9ff224ba145dece37317ad850ae5cc
ashurohom <ashitosh.rohom@gmail.com>
2024-12-03 14:43:55 +0530

    commited

---
+++ MultilevelInheritance.py
@@ -0,0 +1,18 @@
+class A:
+    def aee(self):
+        print(f'Class A')
+class B(A):
+    def bee(self):
+        print(f'Class B')
+class C(B):
+     def cee(self):
+        print(f'Class C')
+class D(C):
+    def dee(self):
+        print(f'Class D')
+
+obj = D()
+obj.aee()
+obj.bee()
+obj.cee()
+obj.dee()

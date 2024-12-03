376665d1cf6b936a1ab80cb6af79780cd40cc715
ashurohom <ashitosh.rohom@gmail.com>
2024-12-03 14:56:40 +0530

    initial commit

---
+++ MultipleInheritance.py
@@ -0,0 +1,18 @@
+class A:
+    def aee(self):
+        print("Class A")
+class B:
+    def bee(self):
+        print("Class B")
+class C:
+    def cee(self):
+        print("Class C")
+class D(A,B,C):
+    def dee(self):
+        print("Class D")
+
+obj = D()
+obj.aee()
+obj.bee()
+obj.cee()
+obj.dee()        

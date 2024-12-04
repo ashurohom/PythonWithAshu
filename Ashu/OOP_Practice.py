9da21f187086bb13f462ad41260ecbafee83c9f1
ashurohom <ashitosh.rohom@gmail.com>
2024-12-05 00:16:41 +0530

    update function

--- Ashu/OOP_Practice.py
+++ Ashu/OOP_Practice.py
@@ -1,4 +1,13 @@
 class A:
-    def __init__(self):
-        print("Class A")
-obj = A()
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+
+    def display(self):    
+        print(f'{self.name},{self.age}')
+
+obj = A("Ashu",24)
+obj.name="Rani"
+# del obj.name
+obj.age=22
+obj.display()

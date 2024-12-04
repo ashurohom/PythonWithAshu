39efeff74e017496caf70500fafd5aaaed95c0b8
ashurohom <ashitosh.rohom@gmail.com>
2024-12-05 00:36:20 +0530

    method update

--- Ashu/OOP_Practice.py
+++ Ashu/OOP_Practice.py
@@ -1,13 +1,23 @@
 class A:
+    bike = "Dream Bike : FZ-S"
+
     def __init__(self,name,age):
         self.name=name
         self.age=age
+        print(f'Constructor : {self.bike}')
 
     def display(self):    
-        print(f'{self.name},{self.age}')
+        print(f'{self.name},{self.age},{self.bike}')
+        print(f'Using Class Name : {A.bike}')
+
+    @classmethod
+    def show(cls):
+        print(f'Class Method : {cls.bike}')
 
 obj = A("Ashu",24)
 obj.name="Rani"
 # del obj.name
 obj.age=22
 obj.display()
+obj.show()
+print(f'Class Variable : {A.bike}')

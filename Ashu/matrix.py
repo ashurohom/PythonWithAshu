ed341b855e19b3a2902a5fe82b6a714def3c9e70
ashurohom <ashitosh.rohom@gmail.com>
2024-12-06 14:11:04 +0530

    initial commit

---
+++ Ashu/matrix.py
@@ -0,0 +1,16 @@
+row = int(input("Enter the number of row : "))
+column = int(input("Enter the number of column : "))
+
+matrix = []
+
+print("Enter the matrix row wise : ")
+for i in range(row):
+    a = []
+    for j in range(column):
+        a.append(int(input("Enter Number : ")))
+    matrix.append(a)
+
+for Row in range(row):
+    for Column in range(column):
+        print(matrix[Row][Column], end = " ")
+    print()    

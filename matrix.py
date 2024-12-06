995dcbae269bfdb75ebc8a4bd80e31d40a408d5b
ashurohom <ashitosh.rohom@gmail.com>
2024-12-07 00:09:15 +0530

    add matrix display loop

---
+++ matrix.py
@@ -0,0 +1,20 @@
+row = int(input("Enter No Of Row : "))
+column = int(input("Enter No Of Column : "))
+
+matrix = []
+
+print("Enter Input : ")
+for i in range(row):
+
+    a = []
+    for j in range(column):
+        a.append(int(input()))
+    matrix.append(a)
+
+for Row in range(row):
+    for Column in range(column):
+        print(matrix[Row][Column], end=" ")
+    print()        
+
+
+ 

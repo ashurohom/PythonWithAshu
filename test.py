42c5cdd4d8d1fc2c156761164510b88dabe24121
ashurohom <ashitosh.rohom@gmail.com>
2025-02-10 20:20:10 +0530

    print primt number

--- test.py
+++ test.py
@@ -38,12 +38,12 @@
 # else:
 #     print("Prime Number")    
 
-# Print Primt Number Between 1 to 25
+# Print Primt Number Between 1 to 100
 
-n=25
+n=100
 for i in range(2,n+1):
     for j in range(2,i):
         if(i%j == 0):
             break
     else:
-        print(j,end=" ")
+        print(i,end=" ")

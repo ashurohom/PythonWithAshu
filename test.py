eebd1d77113eaec7194fbed2e318a843d707f966
ashurohom <ashitosh.rohom@gmail.com>
2025-02-11 19:56:24 +0530

    update primt number code

--- test.py
+++ test.py
@@ -46,9 +46,9 @@ for i in range(2,n+1):
     for j in range(2,i):
         if i % j == 0:
             break
-        else:
-            print(i,end="")
-            
+    else:
+        print(i,end=" ")
+
 
 
         

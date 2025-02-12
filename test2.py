9793b0646af2623ee179717dbb742f4a2eb5007d
ashurohom <ashitosh.rohom@gmail.com>
2025-02-12 20:07:15 +0530

    add list of lowest number

--- test2.py
+++ test2.py
@@ -2,8 +2,13 @@
 
 list = [50,22,48,27,32,18]
 lowestNum = list[0]
+secondLowest = list[0]
 
 for i in list:
     if i < lowestNum:
         lowestNum=i
+
+for i in list:
+    if i > secondLowest and (secondLowest == lowestNum or i < secondLowest): 
+        secondLowest=i
 print(lowestNum)        

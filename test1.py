d650650c5aadf04c105d1d8f9bc6bc3d4d783ed0
ashurohom <ashitosh.rohom@gmail.com>
2025-02-13 19:20:32 +0530

    add test1.py file

---
+++ test1.py
@@ -0,0 +1,21 @@
+# a=1234
+
+# a=(str(a)[::-1])
+# print(int(a))
+
+
+# def ReverseString(s):
+#     if len(s) <= 1:
+#         return s
+#     else:
+#         return ReverseString(s[1:])+s[0]
+# s = input("Enter number : "))    
+# ReverseString(s)
+# print(f'{s} : {ReverseString(s)}')
+
+# class Stud:
+#     name = "Ashu"
+#     RollNo = 111
+# s = Stud();
+# print(s.name)
+# print(s.RollNo)
--- test2.py
+++ test2.py
@@ -13,3 +13,24 @@ for i in list:
         secondLowest=i
 print(lowestNum)        
 print(secondLowest)
+
+
+print("********************************")
+
+
+
+list2 = [50,22,48,27,32,18]
+highestNum = list2[0]
+secondHigh = list2[0]
+
+for i in list2:
+    if i > highestNum:
+        highestNum = i
+
+for i in list2:
+    if i < highestNum and (secondHigh == highestNum or i > secondHigh):
+        secondHigh = i
+
+print(f"highest Number : {highestNum}")        
+print(f"Second High : {secondHigh}")
+

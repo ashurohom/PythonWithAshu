8389d33064a0f50720b193ac83e6efd7ead5f540
ashurohom <ashitosh.rohom@gmail.com>
2025-02-15 22:05:32 +0530

    add prime no

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
-num = 11
+num=29
 
 if num > 1:
     for i in range(2,num):
         if num % i == 0:
-            print("No Prime Number")
+            print("Not Prime")
             break
     else:
         print("Prime Number")    
 else:
-    print("No Prime Number")    
+    print("Not Prime")
